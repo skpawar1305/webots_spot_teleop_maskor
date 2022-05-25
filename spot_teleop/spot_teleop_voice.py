bdf511cb8f26bd1a967b297da9360d215fe38384
Shubham <skpawar1305@gmail.com>
2022-05-25 17:50:19 +0200

    switch to a more reliable wav2vec 2 model
    *reference: https://github.com/oliverguhr/wav2vec2-live

--- spot_teleop/spot_teleop_voice.py
+++ spot_teleop/spot_teleop_voice.py
@@ -8,35 +8,40 @@ from std_msgs.msg import String
 
 import sys
 
-coqui_dir = os.path.join(get_package_share_directory('spot_teleop'), 'coqui_model')
-sys.path.append(coqui_dir)
-from transcribe import STT
+wav2vec2_dir = os.path.join(get_package_share_directory('spot_teleop'), 'wav2vec2')
+sys.path.append(wav2vec2_dir)
+from live_asr import LiveWav2Vec2
+
+model = wav2vec2_dir + '/wav2vec2-large-960h-lv60-self'
 
 
 class TeleopClass(Node):
-    def __init__(self):
+    def __init__(self, asr):
         super().__init__('teleop_voice')
         self.create_timer(0.1, self.mytimercallback)
-        self.stt = STT(coqui_dir)
         spot_name = "Spot"
         self.pub_stt = self.create_publisher(String, '/' + spot_name + '/transcript', 1)
         self.movements_msg = String()
-        self.listen = False
+        self.asr = asr
+        self.asr.start()
 
     def mytimercallback(self):
-        text = self.stt.listen()
-        print('Text:', text)
+        text,sample_length,inference_time = self.asr.get_last_text()                        
+        print(f"{sample_length:.3f}s"
+                +f"\t{inference_time:.3f}s"
+                +f"\t{text}")
         self.movements_msg.data = text
         self.pub_stt.publish(self.movements_msg)
 
 
 def main(args=None):
     rclpy.init()
-    teleop_voice = TeleopClass()
+    asr = LiveWav2Vec2(model, device_name="default")
+    teleop_voice = TeleopClass(asr)
     try:
         rclpy.spin(teleop_voice)
     except KeyboardInterrupt:
-        pass
+        asr.stop()
     teleop_voice.destroy_node()
     rclpy.shutdown()
 
